11d3bae35b09a1877c9a0ac7142da48a20f77049
Guven Bolukbasi <guvenbolukbasi@gmail.com>
2018-07-23 16:08:06 +0300

    basic project setup completed

--- ios-onedrive-starterkit/UI/ViewController.h
+++ ios-onedrive-starterkit/UI/ViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <OneDriveSDK/OneDriveSDK.h>
 
 @interface ViewController : UIViewController
 
--- ios-onedrive-starterkit/UI/ViewController.m
+++ ios-onedrive-starterkit/UI/ViewController.m
@@ -15,15 +15,14 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [ODClient setActiveDirectoryAppId:@"APP_ID" redirectURL:@"REDIRECT_URL"];
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
